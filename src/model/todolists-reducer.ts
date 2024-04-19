1e07673f2c447a4677476e71dcccea169b99ee4a
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-19 20:20:27 +0300

    add export and write todolists-reducer.ts

---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,14 @@
+import { TodolistType } from '../App'
+import { v1 } from 'uuid'
+
+let todolistID1 = v1()
+let todolistID2 = v1()
+
+const initialState: TodolistType[] = [
+   { id: todolistID1, title: 'What to learn', filter: 'all' },
+   { id: todolistID2, title: 'What to buy', filter: 'all' },
+]
+
+export const todolistsReducer = (state: TodolistType[] = initialState, action: any) => {
+   // code
+}
